d775a4e812fd75590e6eb392c4a0d2bc123b7183
DevJasonRoberts <46614652+DevJasonRoberts@users.noreply.github.com>
2019-07-23 09:19:25 -0400

    First Commit

---
+++ bistros/index.php
@@ -0,0 +1,204 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>BISTROS Dine-In </title>
+      <!-- Latest compiled and minified CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
+
+    <!-- jQuery library -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.0/jquery.min.js"></script>
+
+    <!-- Latest compiled JavaScript -->
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="icon" type="image/png" sizes="32x32" href="img/favicon-32x32.png">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+  </head>
+
+  <body>
+    <div class="intro">
+      <nav class="navbar navbar-inverse navbar-fixed-bottom">
+        <div class="container-fluid">
+          <div class="navbar-header">
+            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            </button>
+            <a class="navbar-brand" href="#">
+              <span class="title">BISTROS</span> Jacksonville,FL
+            </a>
+          </div>
+          <div class="collapse navbar-collapse" id="myNavbar">
+
+            <ul class="nav navbar-nav navbar-right">
+              <li class="dropdown">
+                <a class="dropdown-toggle" data-toggle="dropdown" href="#" style="font-family: 'moon';"> Reservations
+                <span class="caret"></span></a>
+                <ul class="dropdown-menu">
+                  <li><a href="#">Call Ahead Reservation</a></li>
+                  <li><a href="#">Dinner Reservation</a></li>
+                  <li><a href="#">Event Banquet Reservation</a></li>
+                </ul>
+              <li class="dropdown">
+                <a class="dropdown-toggle" data-toggle="dropdown" href="#" style="font-family: 'moon';"> Jobs
+                <span class="caret"></span></a>
+                <ul class="dropdown-menu">
+                  <li><a href="#">Management Positions</a></li>
+                  <li><a href="#">Front of House Positions</a></li>
+                  <li><a href="#">Heart of House Positions</a></li>
+                </ul>
+              <li class="dropdown">
+                <a class="dropdown-toggle" data-toggle="dropdown" href="#" style="font-family: 'moon';"> Contact Us
+                <span class="caret"></span></a>
+                <ul class="dropdown-menu">
+                  <li><a href="#">Email Us</a></li>
+                  <li><a href="#">Text Us</a></li>
+                  <li><a href="#">To-Go Ordering</a></li>
+                </ul>
+              <li><a href="#"><span class="glyphicon glyphicon-cellphone"></span>1-800-123-4567</a></li>
+            </ul>
+          </div>
+        </nav>
+
+          <div class="container-fluid">
+            <div class="row">
+              <div class="col-md-12 text-center">
+                  <div class="Subtitle">
+                    <h1 class="name">BISTROS</h1>
+                    <h2>Voted Best New Restaurant In Jacksonville!</h2>
+                    <button type="button" class="btn btn-success subbutton">Order Online!</button>
+                  </div>
+                </div>
+            </div>
+          </div>
+        </div>
+    </div>
+    <div class="container-fluid">
+      <div class="col-md-12 section-light">
+        <p></p>
+      </div>
+    </div>
+
+    <div class="container-fluid menu">
+      <div class="row menutitle">
+        <div class="col-md-12 text-center">
+          <h1 class="menuname">BISTROS</h1>
+          <h1 class="menusubtitle">New Summer Menus Have Launched!</h1>
+        </div>
+      </div>
+    </div>
+    <div class="container-fluid">
+      <div class="col-md-12 section-light">
+        <p></p>
+      </div>
+    </div>
+    <div class="row menus">
+        <div class="col-md-4 breakfast text-center">
+          <h3 class="foodname left">Deluxe Avacado Toast</h3>
+        </div>
+        <div class="col-md-4 lunch text-center">
+          <h3 class="bistros">BISTROS Fiesta Bowl</h3>
+        </div>
+        <div class="col-md-4 dinner text-center">
+          <h3 class="foodname right"> Shrimp and Quail Soup</h3>
+        </div>
+      </div>
+      <br>
+    <div class="row">
+        <div class="col-md-4 text-center">
+          <a class="menulink" href="#"><h3 >Breakfast Menu</h3></a>
+        </div>
+        <div class="col-md-4 text-center">
+          <a class="menulink" href="#"><h3 >Lunch Menu</h3></a>
+        </div>
+        <div class="col-md-4 text-center">
+          <a class="menulink" href="#"><h3 >Dinner Menu</h3></a>
+        </div>
+    </div>
+    <div class="container-fluid">
+      <div class="col-md-12 section-light">
+        <br>
+      </div>
+    </div>
+    <div class="container-fluid">
+      <div class="row">
+        <div class="col-md-6 chefall text-center">
+          <div class="yourchef">
+            <h1 class="cheftitle">Your Chef</h1>
+            <h2 class="chefname">Executive Chef Nick</h2>
+            <h3 class="chefdescription">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+          </div>
+        </div>
+        <div class="col-md-6 reviewtitle text-center">
+          <div class="reviewcategory">
+            <h1>TESTIMONIES</h1>
+          </div>
+          <div class="review">
+            <img src="img/man.jpg" class="peoplepic" alt="">
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat!
+            </p>
+            <h5>Jacksonville Sun July 10, 2018</h5>
+          </div>
+          <div class="review">
+            <img src="img/woman.jpg" class="peoplepic" alt="">
+            <p>
+              Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua!
+            </p>
+            <h5>Jacksonville Sun July 10, 2018</h5>
+          </div>
+        </div>
+        <br>
+      </div>
+    </div>
+      <div class="row">
+          <div class="col-md-6 socialmedia text-left">
+            <div class="socialmedia">
+
+              <a href="#">
+                <i class="fa fa-facebook-square" aria-hidden="true"></i>
+              </a>
+              <a href="#">
+                <i class="fa fa-instagram" aria-hidden="true"></i>
+              </a>
+              <a href="#">
+                <i class="fa fa-github-square" aria-hidden="true"></i>
+              </a>
+              <a href="#">
+                <i class="fa fa-twitter-square" aria-hidden="true"></i>
+              </a>
+            </div>
+          </div>
+            <div class="container-fluid">
+              <div class="col-md-6">
+              <br>
+              </div>
+          </div>
+      </div>
+        <div class="container text-center">
+          <div class="row copyright">
+            <div class="col-md-4">
+              <p>Copyright 2018, BISTROS LLC</p>
+            </div>
+            <div class="col-md-4">
+              <p>Bistros 123 Main St., Jax,Fl</p>
+            </div>
+            <div class="col-md-4">
+              <a class=".policy" href="#">
+                <span>Privacy</span>
+              </a>
+              <a class=".policy" href="#">
+                <span>Policy</span>
+              </a>
+            </div>
+          </div>
+        </div>
+    <br>
+    <br>
+    <br>
+  </body>
+</html>
